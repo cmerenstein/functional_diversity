eb8b77dfb743966aac1d3c27570ab3e71f1dbec9
Carter <cmerenstein@gmail.com>
2023-03-01 11:16:13 -0500

    multiple studies

--- multiple_studies.R
+++ multiple_studies.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(curatedMetagenomicData)
 library(SummarizedExperiment)
 library(vegan)
+library(parallel)
 
 
 ## --- list studies & pick -----
@@ -19,39 +20,29 @@ case_control = sapply(studies, function(stud){
 })
 case_control = case_control[ !(is.na(case_control)) ]
 
+## %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+## -------------- Necessary functions -------------------
 
-get_assigned_taxa = function(pathways) {
-
-    pathways_df =data.frame( pathway = rownames(pathways), pathways)
-    long = gather(pathways_df, "sample", "abundance", -pathway)
-
-    # everything assigned to a taxa
-    assigned = long[ !(grepl("UNINTEGRATED", long$pathway)) & long$pathway!= "UNMAPPED",]
-    assigned = separate(assigned, pathway, c("PWY", "description"), sep = ": ") %>%
-                    separate(description, c("descirption", "taxa"), sep = "\\|", fill = "right") %>%
-                    filter(taxa != "unclassified" & !(is.na(taxa)))
-    return(assigned)
-}
-
-get_pathway_totals = function(pathways){
+clean_pathways= function(pathways) {
+## get the dataframes of pathways/taxa/samples
 
     pathways_df =data.frame( pathway = rownames(pathways), pathways)
-    long = gather(pathways_df, "sample", "abundance", -pathway)
+    tidy = gather(pathways_df, "sample", "abundance", -pathway)
 
     # everything assigned to a taxa
-    assigned = long[ !(grepl("UNINTEGRATED", long$pathway)) & long$pathway!= "UNMAPPED",]
-    totals = separate(assigned, pathway, c("PWY", "description"), sep = ": ") %>%
-                    separate(description, c("descirption", "taxa"), sep = "\\|", fill = "right") %>%
-                    filter(is.na(taxa))
-    return(totals)
+    tidy_clean = tidy[ !(grepl("UNINTEGRATED", tidy$pathway)) & tidy$pathway!= "UNMAPPED",] %>%
+                       separate(pathway, c("PWY", "description", "taxa"), 
+                        sep = ": |\\|", fill = "right")
 
+    assigned = filter(tidy_clean, taxa != "unclassified" & !(is.na(taxa)))
+    totals = filter(tidy_clean, is.na(taxa))
+
+    return(list(assigned, totals))
 }
 
-## takes df of assigned taxa and produces p value of bray dissimilarity
 pathway_bray = function(pwy, assigned, condition){
+## takes df of assigned taxa and produces p value based on permanova w/ bray distances
 
-    print(pwy)
-    
     pathway_long = filter(assigned, PWY == pwy) %>%
                     select(taxa, sample, abundance) %>%
                     spread( sample, abundance)    
@@ -59,65 +50,79 @@ pathway_bray = function(pwy, assigned, condition){
     if (nrow(pathway_mat) == 1) { return(NA)}
 
     ## remove columns with no values, return NA if we no longer have each condition
-    pathway_mat = pathway_mat[, colSums(pathway_mat) > 0 ]
+    pathway_mat = pathway_mat[, colSums(pathway_mat) > 0 , drop = F]
     if (length( unique( condition[ colnames(pathway_mat)])) < 2 |
          ncol(pathway_mat) < 10 ) { return(NA)}
     
     bray = as.matrix(vegdist( t(pathway_mat) ))
 
     set.seed(19104)
-    permanova = adonis2( bray ~ condition[rownames(bray)], permutations = 9999 )
+    permanova = adonis2( bray ~ condition[rownames(bray)], permutations = 999 )
     return( permanova[1, "Pr(>F)"] )    
         
 }
 
 
-## takes df of pathway totals  and produces p value from wilcox test
-pathway_wilcox = function(pwy, totals, condition){
+pathway_kruskal = function(pwy, totals, condition){
+## takes df of pathway totals  and produces p value from kruskal test
 
     pathway_abundance = filter(totals, PWY == pwy) 
 
     if (length( unique( condition[pathway_abundance$sample])) < 2 |
         nrow(pathway_abundance) < 10) {return(NA)} 
 
-    wx = wilcox.test(pathway_abundance$abundance ~ condition[ pathway_abundance$sample ])
+    wx = kruskal.test(pathway_abundance$abundance ~ condition[ pathway_abundance$sample ])
     return(wx$p.value)
 }
 
+## %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ## --- Main loop of studies ----
-for (stud in case_control[1:5]){
+significance_list = lapply( case_control[1:10], function(stud){
     
     d = curatedMetagenomicData(stud, dryrun = F)[[1]]
 
     meta = pData(d)
     pathways = exprs(d)
 
+    ## first remove samples where condition is NA
+    meta = meta[ !(is.na(meta$study_condition)),]
+    athways = pathways[, colnames(pathways) %in% rownames(meta)]
+
     ## because R gets funky sometimes, we have to make sure names are 'proper'
     rownames(meta) = make.names(rownames(meta))
     colnames(pathways) = make.names(colnames(pathways))
     condition = meta$study_condition 
     names(condition) = rownames(meta)
+    
 
-    assigned = get_assigned_taxa(pathways)
-    totals = get_pathway_totals(pathways)
+    clean = clean_pathways(pathways)
+    assigned = clean[[1]]
+    totals = clean[[2]]
 
+    ## get p values
     adonis_p = sapply(unique(assigned$PWY), pathway_bray, assigned = assigned, condition = condition) 
-    wilcox_p = sapply(unique(totals$PWY), pathway_wilcox, totals = totals, condition = condition)
+    kruskal_p = sapply(unique(totals$PWY), pathway_kruskal, totals = totals, condition = condition)
    
-    adonis_df = data.frame(pathway = names(adonis_p), adonis_p, adonis_FDR = p.adjust(adonis_p, "BH")) 
-    wilcox_df = data.frame(pathway = names(wilcox_p), wilcox_p, wilcox_FDR = p.adjust(wilcox_p, "BH")) 
+    ## make into dataframe and merge
+    adonis_df = data.frame(pathway = names(adonis_p), adonis_p, 
+                            adonis_FDR = p.adjust(adonis_p, "BH")) 
+    kruskal_df = data.frame(pathway = names(kruskal_p), kruskal_p, 
+                            kruskal_FDR = p.adjust(kruskal_p, "BH")) 
+    pathways_sig = full_join(adonis_df, kruskal_df, by = "pathway")
 
-    print(head(adonis_df))
-    print(wilcox_df %>% head())
+    pathways_sig$study = stud
+    return(pathways_sig)
+})
 
-    pathways_sig = full_join(adonis_df, wilcox_df, by = "pathway")
+significance_df = do.call("rbind", significance_list)
+saveRDS(significance_df, "sig_1_10.rds")
 
-    cat("adonis: ",  sum(adonis_df$adonis_FDR < .1, na.rm = T), "\n")
-    cat("wilcox:", sum(wilcox_df$wilcox_FDR < .1, na.rm = T), "\n")
-}
+head(arrange(significance_df, adonis_p), 20)
 
+adonis_sig = significance_df %>% filter(adonis_FDR < 0.05)
+rownames(adonis_sig) = NULL
+table(adonis_sig$pathway) %>% sort() %>% tail(20)
 
-
-
-
+kruskal_sig = significance_df %>% filter(kruskal_FDR < 0.05)
+table(kruskal_sig$pathway) %>% sort() %>% tail(20)
 
